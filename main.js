155d73e5192715acb7ab7c861996107fc8fc9225
karart1981 <karapart1981@gmail.com>
2023-07-26 14:43:44 +0400

    I create Ai Audio Generator

---
+++ main.js
@@ -0,0 +1,22 @@
+// Function to generate audio from the entered text
+function generateAudio() {
+    const textToRead = document.getElementById('text-to-read').value;
+    
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(textToRead);
+
+    // speech settings, volume, etc
+    utterance.voice = synth.getVoices().find(voice => voice.name === 'Your Preferred Voice');
+    utterance.rate = 1.0; // Adjust the speech rate
+
+    synth.speak(utterance);
+
+    // Updating audio element source
+    const audioPlayer = document.getElementById('audio-player');
+    audioPlayer.src = URL.createObjectURL(new Blob([new Uint8Array(0)], { type: 'audio/wav' }));
+}
+
+// Attach the event listener to the Generate Audio button
+document.getElementById('generate-audio').addEventListener('click', generateAudio);
+
+   
