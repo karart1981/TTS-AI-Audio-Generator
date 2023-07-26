e6b28effbaca2c8c7592605dfe72b3088f4f1312
karart1981 <karapart1981@gmail.com>
2023-07-26 15:42:19 +0400

    I changed vloue speed

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function generateAudio() {
 
     // speech settings, volume, etc
     utterance.voice = synth.getVoices().find(voice => voice.name === 'Your Preferred Voice');
-    utterance.rate = 1.0; // Adjust the speech rate
+    utterance.rate = 1.5; // Adjust the speech rate
     utterance.pitch = 1.0; // Increase the pitch
     utterance.volume = 1.0; // Decrease the volume
     
